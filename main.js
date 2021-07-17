579a405b561bffa98b574d6654f51ad3cc18cf43
Taurus0205 <dolchin1990@mail.ru>
2021-07-17 12:46:53 +0500

    Add todo list paractice in js

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
-const elHumbergerBtn = document.querySelector(".hamburger-btn");
-const elHeader = document.querySelector(".header");
+var elForm = document.querySelector(".todo-form");
+var elInput = elForm.querySelector(".todo-input");
+var elResult = elForm.querySelector(".result");
 
-elHumbergerBtn.addEventListener("click", function () {
-  elHeader.classList.toggle("header--adoptive");
+elForm.addEventListener("submit", function (evt) {
+  evt.preventDefault();
+
+  var userInput = elInput.value.trim();
+
+  var newLi = document.createElement("li");
+  newLi.setAttribute("class", "todo-item");
+  var newSpan = document.createElement("span");
+  var newBtn = document.createElement("button");
+  newBtn.textContent = "Delete";
+  newBtn.setAttribute("class", "todo-item__btn");
+  newBtn.setAttribute("type", "button");
+
+  todo_list.appendChild(newLi);
+  newLi.appendChild(newSpan);
+  newLi.appendChild(newBtn);
+  newSpan.textContent = userInput;
+  elInput.value = null;
 });
